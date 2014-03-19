4c5422c7d9c7a5dd2bf09a5766f3910b092333d7
rageit <rejanlikesraisin@hotmail.com>
2014-03-19 16:15:08 -0400

    First Commit
    Just including the project files...

---
+++ EulerSolutions/Common/IProblemSolver.cs
@@ -0,0 +1,17 @@
+﻿namespace EulerSolutions.Common
+{
+    public interface IProblemSolver
+    {
+        /// <summary>
+        /// Represents the title of the problem
+        /// </summary>
+        string Title { get; }
+
+        /// <summary>
+        /// This is the definition of the problem
+        /// </summary>
+        string Definition { get; }
+
+        string Solve();
+    }
+}
---
+++ EulerSolutions/Problems/Problem15.cs
@@ -0,0 +1,86 @@
+﻿using System.Globalization;
+using EulerSolutions.Common;
+
+namespace EulerSolutions.Problems
+{
+    public class Problem15 : IProblemSolver
+    {
+        public string Title
+        {
+            get { return "Lattice paths"; }
+        }
+
+        public string Definition
+        {
+            get
+            {
+                return "Starting in the top left corner of a 2×2 grid, " +
+                       "and only being able to move to the right and down, " +
+                       "there are exactly 6 routes to the bottom right corner. " +
+                       "How many such routes are there through a 20×20 grid?";
+            }
+        }
+
+        /// This can be solved by traversing the graph structure recursively. 
+        /// Example of graph for 2x2 matrix
+        ///                  0,0
+        ///             0,1       1,0
+        ///         2,0      1,1       0,2
+        ///             2,0       1,2
+        ///                  2,2
+        public string Solve()
+        {
+            const int maxDim = 2;
+            var baseNode = ConstructGraph(maxDim, maxDim);
+
+            return CountPaths(baseNode, maxDim, maxDim).ToString(CultureInfo.InvariantCulture);
+        }
+
+        private int CountPaths(Node baseNode, int xDim, int yDim)
+        {
+            return 0;
+        }
+
+        private Node ConstructGraph(int xDim, int yDim)
+        {
+            var baseNode = new Node(0, 0);
+            AddNode(baseNode, xDim, yDim);
+
+            return baseNode;
+        }
+
+        private void AddNode(Node currentNode, int xDim, int yDim)
+        {
+            if (currentNode.X < xDim)
+            {
+                var leftChildNode = new Node(currentNode.X + 1, currentNode.Y);
+                currentNode.LeftNode = leftChildNode;
+
+                AddNode(currentNode.LeftNode, xDim, yDim);
+            }
+
+            if (currentNode.Y < yDim)
+            {
+                var rightChildNode = new Node(currentNode.X, currentNode.Y + 1);
+                currentNode.RightNode = rightChildNode;
+
+                AddNode(currentNode.RightNode, xDim, yDim);
+            }
+        }
+
+        class Node
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
+
+            public Node LeftNode { get; set; }
+            public Node RightNode { get; set; }
+
+            public Node(int x, int y)
+            {
+                X = x;
+                Y = y;
+            }
+        }
+    }
+}
