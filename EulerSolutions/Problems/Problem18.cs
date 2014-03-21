6bf282dafa7af75832786aa4103af527c1ae2394
rageit <rejanlikesraisin@hotmail.com>
2014-03-21 16:55:09 -0400

    Changes

--- EulerSolutions/Problems/Problem18.cs
+++ EulerSolutions/Problems/Problem18.cs
@@ -4,8 +4,15 @@ namespace EulerSolutions.Problems
 {
     public class Problem18 : IProblemSolver<string>
     {
-        public string Title { get; private set; }
-        public string Definition { get; private set; }
+        public string Title
+        {
+            get { return "Problem 18: Maximum path sum I"; }
+        }
+
+        public string Definition
+        {
+            get { return "http://projecteuler.net/problem=18"; }
+        }
 
         public string Solve()
         {
