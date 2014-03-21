f4cf30f649897f904dc68a5f724ce1417e8fbe7e
rageit <rejanlikesraisin@hotmail.com>
2014-03-21 15:23:48 -0400

    Adding missing files from last commit

---
+++ EulerSolutions.Tests/NumberToWords.Tests.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+using EulerSolutions.Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EulerSolutions.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void OnesTests()
+        {
+            var numberToWords = new NumberToWords();
+
+            for (int i = 0; i <= 1000; i++)
+            {
+                var w = numberToWords.GetWordsForNumber(i);
+                Debug.WriteLine(w);    
+            }
+        }
+    }
+}
