502bf35c5a94729dddbf312a5708d6001b70faad
rageit <rejanlikesraisin@hotmail.com>
2014-03-21 15:34:15 -0400

    Added file for Problem 18

---
+++ EulerSolutions/Problems/Problem18.cs
@@ -0,0 +1,15 @@
+﻿using EulerSolutions.Common;
+
+namespace EulerSolutions.Problems
+{
+    public class Problem18 : IProblemSolver<string>
+    {
+        public string Title { get; private set; }
+        public string Definition { get; private set; }
+
+        public string Solve()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
